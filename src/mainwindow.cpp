ed1e2e534414b35a720b5112733f445427724aff
Catishere <viktor.gyoshev@gmail.com>
2018-10-05 23:23:29 +0300

    karaoke-init

---
+++ src/mainwindow.cpp
@@ -0,0 +1,161 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QFile config_file("config/hldir.conf");
+
+    config_file.open(QIODevice::ReadOnly);
+
+    hldir = config_file.readAll();
+
+    config_file.close();
+
+    if (!hldir.isEmpty())
+    {
+        ui->startButton->setEnabled(true);
+
+        tracklist.setFileName(hldir + "/lyrics_list.cfg");
+        dest.setFileName(hldir + "/lyricsmaster.cfg");
+    }
+
+    refreshSongList();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+bool MainWindow::refreshSongList()
+{
+    ui->listWidget->clear();
+
+    QDir dir("lyrics");
+    QStringList files = dir.entryList(QDir::Files);
+
+    for (int i = 0; i < files.size(); i++)
+    {
+        ui->listWidget->addItem(files.at(i));
+    }
+    return true;
+}
+
+bool MainWindow::createTracklistFile()
+{
+    QString list("echo \"--------------------------------------------------------\"\n");
+
+    for (int i = 0; i < ui->listWidget->count(); i++)
+    {
+        QString i_str = QString::number(i + 1);
+        list.append("echo \"song" + i_str + ": " + ui->listWidget->item(i)->text() + "\"\n");
+    }
+
+    list.append("echo \"--------------------------------------------------------\"\n");
+
+    tracklist.write(list.toUtf8());
+    return true;
+}
+
+bool MainWindow::addSongToConfig(const QString &filename, const QString &id)
+{
+    QString line;
+    QFile source("lyrics/" + filename);
+
+    source.open(QIODevice::ReadOnly);
+
+    int i = 0;
+
+    for (i = 0; !source.atEnd(); i++) {
+        line = source.readLine();
+        line.remove('"');
+        line = line.trimmed();
+
+        if (!line.isEmpty()) {
+            QString cfg_line = QString("alias song%1lyrics%2 \"say ~ %3 ;alias spamycs song%4lyrics%5\"\n")
+                 .arg(id, QString::number(i), line, id, QString::number(i + 1));
+            dest.write(cfg_line.toUtf8());
+        }
+
+        else i--;
+    }
+
+    QString str = QString("alias song%1lyrics%2 song%3lyrics0;\n")
+                        .arg(id, QString::number(i), id);
+    dest.write(str.toUtf8());
+
+    return true;
+}
+
+bool MainWindow::createSongIndex(const QString &id)
+{
+    QString str("alias song" + id + " \"alias spamycs song" + id + "lyrics0;\n");
+    dest.write(str.toUtf8());
+    return true;
+}
+
+void MainWindow::on_directoryButton_clicked()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                "/",
+                                                QFileDialog::ShowDirsOnly
+                                                | QFileDialog::DontResolveSymlinks);
+
+    if (!dir.isEmpty() && QDir(dir).entryList().contains("config.cfg"))
+    {
+        ui->startButton->setEnabled(true);
+
+        QFile cfg("config/hldir.conf");
+        cfg.open(QIODevice::WriteOnly);
+        cfg.write(dir.toUtf8());
+        cfg.close();
+
+        hldir = dir;
+
+        tracklist.setFileName(hldir + "/lyrics_list.cfg");
+        dest.setFileName(hldir + "/lyricsmaster.cfg");
+    }
+}
+
+void MainWindow::on_refreshButton_clicked()
+{
+    refreshSongList();
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    tracklist.open(QIODevice::WriteOnly | QIODevice::Truncate);
+    dest.open(QIODevice::WriteOnly | QIODevice::Truncate);
+
+    ui->status->setText("Working");
+
+    dest.write("alias spamycs say_team \"type exec lyrics_list.cfg in the console to see list with available songs\"\n");
+    createTracklistFile();
+
+    for (int i = 0; i < ui->listWidget->count(); i++)
+    {
+        addSongToConfig(ui->listWidget->item(i)->text(),QString::number(i + 1));
+        createSongIndex(QString::number(i+1));
+    }
+
+    tracklist.close();
+    dest.close();
+}
+
+void MainWindow::on_addSongButton_clicked()
+{
+    QStringList songs_list = QFileDialog::getOpenFileNames(this, tr("Open Directory"),
+                                                "/", tr("Text Files (*.txt)"));
+    for (int i = 0; i < songs_list.size(); i++)
+    {
+        int pos = songs_list.at(i).lastIndexOf(QChar('/'));
+        QString song = songs_list.at(i);
+
+        QString songname = songs_list.at(i).left(pos);
+    }
+    refreshSongList();
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QDir>
+#include <QFileDialog>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    bool createTracklistFile();
+    bool addSongToConfig(const QString &filename, const QString &id);
+    bool createSongIndex(const QString &id);
+    bool refreshSongList();
+
+private slots:
+
+    void on_directoryButton_clicked();
+
+    void on_refreshButton_clicked();
+
+    void on_startButton_clicked();
+
+    void on_addSongButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString hldir;
+    QFile tracklist;
+    QFile dest;
+};
+
+#endif // MAINWINDOW_H
